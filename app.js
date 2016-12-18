b7bf0b5276efbb5173880bbc5c8a25f65939150c
Nick Papasavvas <nikospapasavvas@gmail.com>
2016-12-18 17:42:39 +0200

    Fix display on connection, fix hAxis dates

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const express      = require('express'),
       yahooFinance =  require('yahoo-finance'),
       PORT         = process.env.PORT || 3000;    
 
-
 const handlebars      = require('express-handlebars').create({
         defaultLayout: 'main',
         helpers:{
@@ -19,122 +18,27 @@ const handlebars      = require('express-handlebars').create({
     });
 var stocksGlobal       = [],
     addRowsArrayGlobal = [],
-    i                  = 0 ;
-
-var stockSymbols = ["DAY"];
-var dataforGoogleChart = [];
-
-
-//
-//var getData = function(req,res, next){
-//  yahooFinance.historical({
-//  symbol: req.params.stockSymbol,
-//  from: '2016-10-01',
-//  to: '2016-11-14',
-//  // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-//}, function (err, quotes) {
-//  if (err) console.log("Error Message from routes.js:" + err );
-//  else if (quotes.length===0) { 
-//      //res.render('home',{"stocks":stocksGlobal, "prices" : JSON.stringify(addRowsArrayGlobal)});
-//      next();
-//  }
-//  else if (quotes.length!==0 ){ 
-//      console.log(quotes);
-//      let j=1;
-//      let k=0;
-//      quotes.forEach(function(elem){   
-//        let rArray      = [];
-//          if (i===0  && stocksGlobal.indexOf(req.params.stockSymbol)===-1 ){ rArray.push(j); j++; 
-//            rArray.push(elem.adjClose);
-//            addRowsArrayGlobal.push(rArray)}
-//          
-//          else if (i!==0 && stocksGlobal.indexOf(req.params.stockSymbol)===-1 ) {
-//            addRowsArrayGlobal[k].push(elem.adjClose);
-//            k++;  
-//          }
-//      })
-//      i++ ;
-//  
-//      if ( stocksGlobal.indexOf(req.params.stockSymbol)===-1 ){
-//      stocksGlobal.push(req.params.stockSymbol)}
-//     // console.log({"stocks": stocksGlobal, "prices":addRowsArrayGlobal });
-////      http://stackoverflow.com/questions/33979290/pass-array-of-arrays-from-routes-to-view-node-js
-//      if (quotes.length!==0 ){
-//      //res.render('home',{"stocks":stocksGlobal, "prices" : JSON.stringify(addRowsArrayGlobal)});
-//          next();
-//      }
-//      }
-//});
-//}
-
-//var getData = function(req,res, next){
-//  yahooFinance.historical({
-//  symbol: req.params.stockSymbol,
-//  from: '2016-11-07',
-//  to: '2016-11-14',
-//  // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-//}, function (err, quotes) {
-//  if (err) console.log("Error Message from routes.js:" + err );
-//  else if (quotes.length===0) { 
-//      //res.render('home',{"stocks":stocksGlobal, "prices" : JSON.stringify(addRowsArrayGlobal)});
-//      
-//  } //if it is the first time the function is called
-//  else if (quotes.length!==0 && dataforGoogleChart.length===0){
-//      dataforGoogleChart.push(['Date',req.params.stockSymbol]);
-//      let j=1;
-//      //console.log(quotes);
-//      quotes.forEach(function(elem){
-//          console.log('inside first loop');
-//          var row = [];
-//          row.push(j,elem.adjClose);
-//          dataforGoogleChart.push(row);
-//          j++;
-//      });
-//           
-//  }
-//   //if it is the not the first time the function is called
-//  else if (quotes.length!==0 && dataforGoogleChart.length>0){ 
-//      dataforGoogleChart[0].push(req.params.stockSymbol);
-//      let j=1;
-//      console.log(quotes);
-//      console.log('inside second loop');
-//      quotes.forEach(function(elem){
-//          dataforGoogleChart[j].push(elem.adjClose);
-//          j++;
-//      });
-//      
-//      };           
-//
-//    console.log('data for google');
-//    console.log(dataforGoogleChart);
-//    next();
-//  
-//  });
-//  
-//}
-
-
-
+    i                  = 0 ,
+    stockSymbols       = ["DAY"],
+    dataforGoogleChart = [];
 var getData = function(stockSymbol){
   yahooFinance.historical({
   symbol: stockSymbol,
-  from: '2016-11-07',
-  to: '2016-11-14',
+  from: new Date(new Date().setDate(new Date().getDate() - 31)),
+  to: new Date,
   // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
 }, function (err, quotes) {
   if (err) console.log("Error Message from routes.js:" + err );
   else if (quotes.length===0) { 
-      //res.render('home',{"stocks":stocksGlobal, "prices" : JSON.stringify(addRowsArrayGlobal)});
       
   } //if it is the first time the function is called
   else if (quotes.length!==0 && dataforGoogleChart.length===0){
       dataforGoogleChart.push(['Date',stockSymbol]);
       let j=1;
-      //console.log(quotes);
       quotes.forEach(function(elem){
-          console.log('inside first loop');
           var row = [];
-          row.push(j,elem.adjClose);
+          var  d  = (  String(elem.date).slice(4,10) )
+          row.push(d ,elem.adjClose);
           dataforGoogleChart.push(row);
           j++;
       });
@@ -144,8 +48,6 @@ var getData = function(stockSymbol){
   else if (quotes.length!==0 && dataforGoogleChart.length>0){ 
       dataforGoogleChart[0].push(stockSymbol);
       let j=1;
-      console.log(quotes);
-      console.log('inside second loop');
       quotes.forEach(function(elem){
           dataforGoogleChart[j].push(elem.adjClose);
           j++;
@@ -156,11 +58,10 @@ var getData = function(stockSymbol){
     console.log(dataforGoogleChart);
     io.sockets.emit('newStockDataAvailable', dataforGoogleChart); 
   });
-  
+  if (!stockSymbol){
+       io.sockets.emit('newStockDataAvailable', dataforGoogleChart);
+  }
 }
-
-
-
 
 var stocksRequested = [];
 app.engine('handlebars', handlebars.engine);
@@ -168,26 +69,19 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname+ '/public'));
 //https://code.tutsplus.com/tutorials/real-time-chat-with-nodejs-socketio-and-expressjs--net-31708
 app.get('/', function(req, res){
-    
     console.log('stocks global root');
            console.log(stocksGlobal);
      res.render('home',{'data': JSON.stringify(dataforGoogleChart)}); 
 } );
 
-
     io.on('connection', function(socket){
     console.log('User connected via socket.io!');
-    socket.emit('welcomeMessage',  stocksRequested); 
+    socket.emit('newStockDataAvailable', dataforGoogleChart ); 
        socket.on('newStockRequest', function (newStock) {
            getData(newStock);
- 
-          // io.sockets.emit('newStockDataAvailable', newData);
     });
 });
   
-
 http.listen(PORT, function(){
     console.log('Express listening on port '+ PORT + '!');
 });
-
-  
